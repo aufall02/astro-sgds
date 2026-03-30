0cfdd531ff86d35fd76677201e887226527efb24
aufall02 <aufaluzumaki@gmail.com>
2026-03-30 19:43:43 +0700

    konfigurasi deploy to cloudflare worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -10,4 +12,6 @@ export default defineConfig({
       noExternal: ["@govtechsg/sgds-web-component"],
     },
   },
+
+  adapter: cloudflare(),
 });
